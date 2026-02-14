b69dda23e68ba4ac6386bf0a2febcc920ea1c840
Jefgit <riazondajf@gmail.com>
2026-02-14 22:53:33 +0800

    remove error lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from "./components/Layout"
-<<<<<<< HEAD
-=======
-
->>>>>>> 2b50e2253d3b9634b282acd1bb61f768184b8867
 
 export default function App() {
 
